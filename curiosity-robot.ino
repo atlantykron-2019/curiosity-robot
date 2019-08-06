d29e49135fbd4981e01f8e5d0a8454479dd603ab
Paul Paraschiv <paul.f.paraschiv@gmail.com>
2019-08-06 11:33:05 +0300

    Realizat conceptul proiectului de baza

--- curiosity-robot.ino
+++ curiosity-robot.ino
@@ -1,11 +1,33 @@
 #include "src/Vehicle.h"
 
+int state=0;
+
 Vehicle curiosity(3, 4, 2, 1);
 
-void setup() {
-  curiosity.test();
+void setup() 
+{
+  Serial.begin(9600);
+  //curiosity.test();
+
 }
 
-void loop() {
+void loop() 
+{
+  if(Serial.available() > 0)
+  { 
+    state = Serial.read(); 
+  }
+  uint8_t i;
+  
+  if (state == 'w')
+    Vehicle forward();
+  if (state == 's')
+    Vehicle backwards();
+  if (state == 'a')
+    Vehicle left();  
+  if (state == 'd')
+    Vehicle right();
+  if (state == 'p')
+    Vehicle stop();  
 
 }
